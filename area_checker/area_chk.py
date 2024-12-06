348174f518f47b2ba2100d5f539d3ac072c1a16e
Rohan maharjan <126095500+sinominee@users.noreply.github.com>
2024-12-06 20:41:57 +0545

    Update area_chk.py

--- area_checker/area_chk.py
+++ area_checker/area_chk.py
@@ -8,6 +8,6 @@ def paint_calc(height, width, cover):
 
 #dont change the code below 
 test_h = int(input("Height of wall: "))
-test_w = int(input("Weight of wall: "))
+test_w = int(input("Width of wall: "))
 coverage = 5
 paint_calc(height=test_h, width=test_w, cover=coverage)
